3f32244feaec305c73d0b7c4ceedcbaf63af6a10
Brucelee Nguyen <bruceleenguyen95@gmail.com>
2018-12-03 20:14:16 -0700

    added SQLTest.cs

---
+++ SE2Final/SE2Final/SQLTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace sqltest
+{
+    class SQLTest
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                String connectionString = "Server=tcp:se2final.database.windows.net,1433;Initial Catalog=SE2Final;Persist Security Info=False;User ID=se2final;Password=@@abc123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT * ");
+                    sb.Append("FROM group_table ");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1} {2} {3} {4}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetBoolean(4));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
