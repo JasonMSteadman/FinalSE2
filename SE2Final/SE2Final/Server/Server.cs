789c7e9fd25f4c1936db8a0a6944a12eca9d85a9
JasonMSteadman <jason.m.steadman@gmail.com>
2018-12-05 18:33:17 -0700

    Finished project

---
+++ SE2Final/SE2Final/Server/Server.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using SE2Final.Classes;
+using System.Collections.Generic;
+using System;
+
+namespace SE2Final
+{
+    public class Server : Hub
+    {
+        //private static Dictionary<string, Dictionary<int, DocFile>> group;
+        //private static Dictionary<int, DocFile> content;
+        private SqlStatements db = new SqlStatements();
+
+
+        /// <summary>
+        /// Standard constructor.
+        /// </summary>
+        public Server() { }
+ 
+        /// <summary>
+        /// Loads page one doc at a time.
+        /// </summary>
+        /// <param name="groupName">Name of the group the doc belows to.</param>
+        /// <returns></returns>
+        public async Task FirstContact(string groupName)
+        {
+            //  Get groups from DB
+            List<DocFile> group = db.QDB(groupName);
+
+            //  Load page one doc at a time
+            foreach(DocFile d in group)
+            {
+                await Clients.Caller.SendAsync("LoadPage", d.iID, d.sTitle, d.sBody, d.iLock, groupName);
+            }   
+        }
+
+        /// <summary>
+        /// Populates group box.
+        /// </summary>
+        /// <returns></returns>
+        public async Task LoadGroupBox()
+        {
+            List<string> group = db.QDBGroupNames();
+
+            foreach(string s in group)
+            {
+                await Clients.Caller.SendAsync("PopGroup", s, false);
+            }
+        }
+
+        /// <summary>
+        /// Edits document title.
+        /// </summary>
+        /// <param name="id">Documents id, this is provided by the html.</param>
+        /// <param name="title">New title.</param>
+        /// <param name="groupName">Name of the group the doc belows to.</param>
+        /// <returns></returns>
+        public async Task NewTitle(int id, string title, string groupName)
+        {
+            //  Update DB
+            db.UpdateTitle(id, title);
+            //  Propagate changes
+            await Clients.All.SendAsync("updateTitle", id, title, groupName);
+        }
+
+        /// <summary>
+        /// Deletes doc.
+        /// </summary>
+        /// <param name="id">Documents id, this is provided by the html.</param>
+        /// <param name="groupName">Name of the group the doc belows to.</param>
+        /// <returns></returns>
+        public async Task DeleteDoc(int id, string groupName)
+        {
+            db.DeleteDoc(id);
+
+            await Clients.All.SendAsync("deletingDoc", id, groupName);
+        }
+
+        /// <summary>
+        /// Creates a new document.
+        /// </summary>
+        /// <param name="title">Document title.</param>
+        /// <param name="groupName">Name of the group the doc belows to.</param>
+        /// <returns></returns>
+        public async Task CreateDoc(string title, string groupName)
+        {
+            DocFile d = db.CreateDoc(groupName, title);
+
+            await Clients.All.SendAsync("LoadPage", d.iID, d.sTitle, d.sBody, d.iLock, groupName);
+        }
+
+        /// <summary>
+        /// Deletes group from the select tag with the id groupbox.
+        /// </summary>
+        /// <param name="groupName">Name of the group the doc belows to.</param>
+        /// <returns></returns>
+        public async Task DeleteGroup(string groupName) 
+        {
+            db.DeleteGroup(groupName);
+            await Clients.All.SendAsync("removeGroup", groupName);
+        }
+        /// <summary>
+        /// Updates the body of a doc.
+        /// </summary>
+        /// <param name="id">Documents id, this is provided by the html.</param>
+        /// <param name="body">New character(s) to add to the body of the selected document.</param>
+        /// <param name="groupName">Name of the group the doc belows to.</param>
+        /// <returns></returns>
+        public async Task BodyTyping(int id, string body, string groupName) 
+        {
+            db.AddToBody(id, body);
+            //group[groupName][id].sBody = body;
+
+            await Clients.All.SendAsync("UpdateBody", "bdy" + id, body, groupName);
+        }
+
+        /// <summary>
+        /// Creates a new group
+        /// </summary>
+        /// <param name="groupName">Name of the group the doc belows to.</param>
+        /// <returns></returns>
+        public async Task CreateGroup(string groupName)
+        {
+            if(!db.CreateGroup(groupName))
+            {   
+                //  Return to an error
+                string message = "A group with the name " + groupName + " already exists.";
+                await Clients.Caller.SendAsync("error", message);
+                return;
+            }
+
+            //  Create new local group
+            //group.Add(groupName, new Dictionary<int, DocFile>());
+            await Clients.Caller.SendAsync("PopGroup", groupName, true);
+            await Clients.Others.SendAsync("PopGroup", groupName, false);
+        }
+
+        /// <summary>
+        /// Checks and updates locks
+        /// </summary>
+        /// <param name="lockID">An id used to identify the div being locked.  This is provided by the html.</param>
+        /// <param name="lckState">0 -> free, 1 -> locked, 2 -> current user owns the lock.</param>
+        /// <param name="groupName">Name of the group the doc belows to</param>
+        /// <returns></returns>
+        public async Task docLock(string lockID, string lckState, string groupName)
+        {
+            int iID = 0;
+            if (lckState.Equals("Locked"))
+                return;
+
+            if (!Int32.TryParse(lockID, out iID))
+                return;
+
+            if (lckState.Equals("Done"))
+            {
+                db.UpdateLock(iID, 0);
+                //group[groupName][iID].iLock = 0;
+                await Clients.All.SendAsync("LockUpdate", iID, 0, groupName);
+                return;
+            }
+
+            if(!db.UpdateLock(iID, 1))
+            {
+                return;
+            }
+            await Clients.Caller.SendAsync("LockUpdate", iID, 2, groupName);
+            await Clients.Others.SendAsync("LockUpdate", iID, 1, groupName);
+        }
+    }
+}
---
+++ SE2Final/SE2Final/SqlStatements.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using SE2Final.Classes;
+
+namespace SE2Final
+{
+
+    // This class will hold all the SQL statements
+    class SqlStatements
+    {
+        //UpdateDataBase("select * from FROM group_table;");
+
+        private const string  conString = "Server=tcp:jsteadmana1.database.windows.net,1433;Initial " +
+                    "Catalog=person99;Persist Security Info=False;User ID=Website;Password=password1!;" +
+                    "MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        public SqlStatements() { }
+
+        /// <summary>
+        /// Updates documents title in db.
+        /// </summary>
+        /// <param name="iID">Document id.</param>
+        /// <param name="sNewTitle">New title for the document.</param>
+        public void UpdateTitle(int iID, string sNewTitle)  ////////////////////////
+        {
+            UpdateDataBase(
+                "UPDATE groupDoc " +
+                "SET docTitle = '" + sNewTitle + "' " +
+                "WHERE docID = " + iID + ";"
+                );
+        }
+
+        /// <summary>
+        /// Gathers all the data for a single group.
+        /// </summary>
+        /// <param name="sGroupName">Group name.</param>
+        /// <returns>Returns a list of DocFiles that are in the DB.</returns>
+        public List<DocFile> QDB(string sGroupName)
+        {
+            return GetFromDataBase(
+                "SELECT * " +
+                "   FROM groupDoc " +
+                "WHERE groupName = '" + sGroupName + "';");
+        }
+
+        /// <summary>
+        /// Gathers all the names within the database
+        /// </summary>
+        /// <returns>Returns a list of group names.</returns>
+        public List<string> QDBGroupNames()
+        {
+            return GetGNFromDataBase("SELECT groupName FROM groupName;");
+        }
+
+        /// <summary>
+        /// Updates the state of the lock.
+        /// </summary>
+        /// <param name="iID">ID of the lock.</param>
+        /// <param name="iNewState">The state you wish to place the lock in. e.g. 0 -> unlocked, 1 -> locked.</param>
+        /// <returns>Whether your action was completed</returns>
+        public bool UpdateLock(int iID, int iNewState)
+        {
+            if(iNewState == 1)
+            {
+               DocFile temp = GetFromDataBase("SELECT * FROM groupDoc WHERE docID = " + iID + ";")[0];
+                if (temp.iLock != 0)
+                    return false;
+            }
+
+            UpdateDataBase("UPDATE groupDoc " +
+                            "    SET lock = " + iNewState + " " +
+                            "WHERE docID = " + iID + ";");
+            return true;
+        }
+
+        /// <summary>
+        /// Deletes a document form the database
+        /// </summary>
+        /// <param name="iID">Document ID.</param>
+        public void DeleteDoc(int iID)
+        {
+            UpdateDataBase("DELETE FROM groupDoc WHERE docID = " + iID + ";");
+        }
+
+        /// <summary>
+        /// Adds a document to the DB.
+        /// </summary>
+        /// <param name="sGroupName">Name of the group the document will be placed.</param>
+        /// <param name="sTitle">Title of the document</param>
+        /// <returns>Returns a copy of the document.</returns>
+        public DocFile CreateDoc(string sGroupName, string sTitle)
+        {
+            UpdateDataBase(
+               "INSERT INTO groupDoc (groupName, docTitle, body, lock)" +
+                    "VALUES('" + sGroupName + "', '" + sTitle + "', '', 0);");            
+
+            return GetFromDataBase("SELECT TOP 1 * " +
+                                   "     FROM groupDoc " +
+                                   "ORDER BY docID DESC")[0];
+        }
+
+        /// <summary>
+        /// Deletes a group from the DB.
+        /// </summary>
+        /// <param name="sGroupName">Name of the group you wish to delete.</param>
+        public void DeleteGroup(string sGroupName)
+        {
+            //  Delete all group members
+            UpdateDataBase("DELETE FROM groupDoc WHERE groupName = '" + sGroupName + "';");
+            
+            //  Delete group
+            UpdateDataBase("DELETE FROM groupName WHERE groupName = '" + sGroupName + "';");
+        }
+
+        /// <summary>
+        /// Create a group in the DB.
+        /// </summary>
+        /// <param name="sGroupName">The Name you would like to try to name your group.</param>
+        /// <returns></returns>
+        public bool CreateGroup(string sGroupName)
+        {
+            List<string> groupList = QDBGroupNames();
+
+            //  Check for name
+            if (groupList.Contains(sGroupName))
+                return false;
+
+            UpdateDataBase("INSERT INTO groupName VALUES('" + sGroupName + "');");
+            return true;
+        }
+
+        /// <summary>
+        /// Adds strings to the DB as they are typed.
+        /// </summary>
+        /// <param name="iID">Document ID.</param>
+        /// <param name="sBody">String being placed into the DB.</param>
+        public void AddToBody(int iID, string sBody)
+        {
+            string sNewBody = GetFromDataBase("SELECT * FROM groupDoc WHERE docID = " + iID + ";")[0].sBody + sBody;
+
+            UpdateDataBase(
+                            "UPDATE groupDoc " +
+                            "SET body = '" + sNewBody + "' " +
+                            "WHERE docID = " + iID + ";"
+                            );
+        }
+
+        /// <summary>
+        /// Helper method that controls all DB updates.
+        /// </summary>
+        /// <param name="sSQLStatement">Statement used to update the DB.</param>
+        private void UpdateDataBase(string sSQLStatement)
+        {
+            //Add user input to database
+            using (SqlConnection dbConn = new SqlConnection())
+            {
+                //Open connection
+                dbConn.ConnectionString = conString;
+                dbConn.Open();
+
+                //Create SQL statement
+                SqlCommand dbQ = new SqlCommand(sSQLStatement, dbConn);
+                //Execute statement
+                try
+                {
+                    dbQ.ExecuteNonQuery();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Helper method that controls all DB queries.
+        /// </summary>
+        /// <param name="sSQLStatement">Statement used to gather data from the DB.</param>
+        /// <returns></returns>
+        private List<DocFile> GetFromDataBase(string sSQLStatement)
+        {
+            List<DocFile> docRet = new List<DocFile>();
+            //Get user input
+            using (SqlConnection dbConn = new SqlConnection())
+            {
+                //Open connection
+                dbConn.ConnectionString = conString;
+
+
+                dbConn.Open();
+
+                SqlCommand getInput = new SqlCommand(sSQLStatement, dbConn);
+
+                //Open reader
+                try
+                {
+                    SqlDataReader dbReader = getInput.ExecuteReader();
+                    dbReader.Read();
+                    while (dbReader.HasRows)
+                    {
+                        DocFile temp = new DocFile(dbReader.GetInt32(0));
+
+                        temp.sGroupName = dbReader.GetString(1);
+                        temp.sTitle = dbReader.GetString(2);
+                        temp.sBody = dbReader.GetString(3);
+                        temp.iLock = dbReader.GetInt32(4);
+                        docRet.Add(temp);
+
+                        try
+                        {
+                            dbReader.Read();
+                        }
+                        catch (Exception e)
+                        {
+                            break;
+                        }
+                    }
+                    //Close db connection
+                    dbReader.Close();
+                }
+                catch(InvalidOperationException ioe)
+                {
+                    return docRet;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return docRet;
+        }
+
+        private List<string> GetGNFromDataBase(string sSQLStatement)
+        {
+            List<string> docRet = new List<string>();
+            //Get user input
+            using (SqlConnection dbConn = new SqlConnection())
+            {
+                //Open connection
+                dbConn.ConnectionString = conString;
+
+                dbConn.Open();
+
+                SqlCommand getInput = new SqlCommand(sSQLStatement, dbConn);
+
+                //Open reader
+                try
+                {
+                    SqlDataReader dbReader = getInput.ExecuteReader();
+
+                dbReader.Read();
+                while (dbReader.HasRows)
+                {
+                    string sGroupName = dbReader.GetString(0);
+                    docRet.Add(sGroupName);
+
+                    try
+                    {
+                        dbReader.Read();
+                    }
+                    catch (Exception e)
+                    {
+                        break;
+                    }
+                }
+
+                //Close db connection
+                dbReader.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return docRet;
+        }
+    }
+}
